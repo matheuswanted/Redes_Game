0af121343c4bc295025d146bb9842f64ac96c89b
Laboratório de Redes <eduardo.curcino@hotmail.com>
2017-06-09 18:57:20 -0300

    init server

---
+++ Server/player.py
@@ -0,0 +1,6 @@
+
+class Player:
+
+    def __init__(self, name, ip):
+        self.name = name
+        self.ip = ip
---
+++ Server/server.py
@@ -0,0 +1,34 @@
+from player import *
+
+class Server:
+
+    def __init__(self):
+        self.rooms = []
+        self.players = []
+
+    def loginPlayer(self, name, ip):
+        # TODO
+
+    def check(self, item):
+        # TODO
+
+    def move(self, direction):
+        # TODO
+
+    def take(self, item):
+        # TODO
+
+    def drop(self, item):
+        # TODO
+
+    def inventory(self):
+        # TODO
+
+    def use(self, item):
+        # TODO
+
+    def speek(self, message):
+        # TODO
+
+    def whisper(self, player, message):
+        # TODO
